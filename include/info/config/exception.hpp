4ddde0de27c8c52ec6a1d82ae6d1cd1f42b21461
bodand <bodand@pm.me>
2020-02-03 22:04:35 +0100

    Reset repository for rewrite and completely different semantics.
     - Removed all source files and headers
     - Abolished dependency on Apache Xerces-C; deleted all correlated files
     - Reformat CMakeLists.txt for a more modular future usage
     - Changed README to POD format for I love POD and ereyesterday
       I learned GitHub supports it as a README format
     - Completely rewritten README.pod; contents are relevant for once
    
    Signed-off-by: bodand <bodand@pm.me>

--- include/info/config/exception.hpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by tudom on 2020-01-20.
-//
-
-#pragma once
-
-#include "exception/InfoConfigException.hpp"
-#include "exception/InitializationException.hpp"
-#include "exception/ParsingException.hpp"
